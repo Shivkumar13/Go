b5d30a4c057aaa0cd0d2383a0e7bae4cedb30d84
shivkumar13 <shivkumar.ople97@gmail.com>
2022-01-13 17:17:59 +0530

    updated with getSum() 04_functions

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -9,6 +9,12 @@ func Greetings(name string) string {
 	return "Hello " + name
 }
 
+func getSum(num1 int, num2 int) int {
+	return num1 + num2
+
+}
 func main() {
 	fmt.Println(Greetings("Shivkumar"))
+	fmt.Println(getSum(4, 5))
+
 }
