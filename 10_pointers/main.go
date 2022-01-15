d3feaea3f6e5b7bd2d4297c04df3eafd2fbdff8b
shivkumar13 <shivkumar.ople97@gmail.com>
2022-01-15 06:36:01 +0530

    updating Pointers demo

--- 10_pointers/main.go
+++ 10_pointers/main.go
@@ -17,6 +17,7 @@ func main() {
 
 	fmt.Println(**b) // So we do **b, which will show the `stored int value`
 
-	fmt.Println(*a) // Grabbing out value from the address.
+	fmt.Println(*a)  // Grabbing out value from the address.
+	fmt.Println(*&c) // This is same as `*a`, because `a := &c`
 
 }
