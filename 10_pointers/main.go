4fbfe0e4ff7901244f152d36453005973e4cbcc8
shivkumar13 <shivkumar.ople97@gmail.com>
2022-01-15 06:33:40 +0530

    Adding Pointers demo

---
+++ 10_pointers/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	c := 4
+	a := &c
+	b := &a
+
+	fmt.Println(a, b)
+	fmt.Printf("%T \t %T\n", a, b)
+
+	//Use * to read the value from address
+
+	fmt.Println(*b) // Showing value stored in `a` which is again and address of `c`
+
+	fmt.Println(**b) // So we do **b, which will show the `stored int value`
+
+	fmt.Println(*a) // Grabbing out value from the address.
+
+}
